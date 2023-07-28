8f5f1a5f71565f91f8a475f58853c54be556fba0
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2023-07-28 10:47:35 -0400

    Solution

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,12 @@
 # your code goes here!
+def countdown(number):
+    while number > 0:
+        print(f"{number} SECONDS(S)!")
+        number -= 1
+    if number == 0:
+        print("HAPPY NEW YEAR")
+    
+
+
+
+countdown(10)
